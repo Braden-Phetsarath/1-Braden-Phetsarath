a4205717ec3782a3dfa0c70f639a3f293414a3c7
Braden-Phetsarath <bkphetsarath@students.unwsp.edu>
2025-09-07 15:37:49 -0500

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -4,3 +4,13 @@ def proverbs_22_29():
     
 
 proverbs_22_29()
+#Braden Phetsarath
+#9/2/2025
+# proverbs 22:29
+def proverbs_22_29():
+    print('"Do you see a man skillful in his work?')
+    print('He will stand before kings;')
+    print('he will not stand before obscure men.”')
+    print('Proverbs 22:29')
+
+proverbs_22_29()
